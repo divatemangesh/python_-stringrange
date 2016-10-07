9c35884cd45c3892b51c47dae1585e22cfe13c66
mangesh divate <divatemangesh@users.noreply.github.com>
2016-10-07 11:26:04 +0530

    commented test prints
    6)   #print(A) # this is for testing  
    10) #print(A)

--- var.py
+++ var.py
@@ -1,12 +1,12 @@
 def r(a,b):
-     A = ord(a)
-     B = ord(b)
+     A = ord(a) # writes integer equvalent of a
+     B = ord(b) # writes integer equvalent of b
      C = B - A
      list1 = []
-     print(A)
+     #print(A) # this is for testing  
      list1.append(chr(A))
      for i in range(C):
        A=A+1
-       print(A)
+       #print(A)
        list1.append(chr(A))
      return list1
